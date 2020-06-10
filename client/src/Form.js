6ddd32d2999fa25f1201d10d9d0790d821b872f3
Barry Yu <doung1118@gmail.com>
2020-06-11 00:33:34 +0800

    first

---
+++ client/src/Form.js
@@ -0,0 +1,165 @@
+import React, { Component } from 'react'
+
+import axios from 'axios';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+//53:00
+
+export default class Form extends Component {
+
+  // 40:21
+
+  state = {
+    name: '',
+    lastname: '',
+    email: '',
+    message: '',
+    sent: false
+  }
+
+  //handle inputs
+  handleName = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      // name: e.target.name
+      name: e.target.value
+    })
+  }
+
+  // setState is change  "state of content "//
+  //this 是指component 
+
+  //end of handle inputs 
+
+  handleEmail = (e) => {
+    this.setState({
+      // email: e.target.email
+      email: e.target.value
+    })
+  }
+
+
+
+  handleMessage = (e) => {
+    this.setState({
+      // message: e.target.message
+      message: e.target.value
+    })
+  }
+
+
+  handleLastname = (e) => {
+    this.setState({
+      // lastname: e.target.lastname
+      lastname: e.target.value
+
+    })
+  }
+
+
+
+  // 54:00
+  formSubmit = (e) => {
+    e.preventDefault();
+
+    let data = {
+      name: this.state.name,
+      lastname: this.state.lastname,
+      email: this.state.email,
+      message: this.state.message
+    }
+    axios.post('http://localhost:5000/api/forma', data)
+      .then(res => {
+        this.setState({
+          sent: true,
+        }, this.resetForm())
+      }).catch(() => {
+        console.log('message not sent');
+      })
+  }
+
+  //for resting init data 
+  // 1:00
+  resetForm = () => {
+    this.setState({
+      name: '',
+      lastname: '',
+      email: '',
+      message: '',
+    })
+
+  }
+
+
+
+  render() {
+    return (
+
+      <div className="container">
+        <form onSubmit={this.formSubmit}>
+          {/* single item */}
+          <div className="singleItem">
+            <label htmlFor="name">name</label>
+            <input type="text" name="name" className="name"
+              placeholder="your name..."
+
+              // 45:13 tuber
+              value={this.state.name}
+              //compont state of handleName // 
+
+              onChange={this.handleName}
+            //onChange 的Ｃ是大寫// 
+
+            />
+          </div>
+          {/* end of single item   */}
+
+          {/* single item */}
+          <div className="singleItem">
+            <label htmlFor="name">last name </label>
+            <input type="text" name="name" className="name"
+              placeholder="your last name..."
+
+              value={this.state.lastname}
+              onChange={this.handleLastname}
+
+            />
+          </div>
+          {/* end of single item   */}
+
+          {/* single item */}
+          <div className="singleItem">
+            <label htmlFor="name">email</label>
+            <input type="text" name="name" className="name"
+              placeholder="your email..."
+
+              value={this.state.email}
+              onChange={this.handleEmail}
+
+            />
+          </div>
+          {/* end of single item   */}
+
+          {/* single item */}
+          <div className="textArea singleItem">
+            <label htmlFor="message">Message</label>
+            <textarea name="message" id=""
+              cols="30" rows="5" placeholder="your Message..."
+
+
+              value={this.state.message}
+              onChange={this.handleMessage}
+
+            ></textarea>
+          </div>
+          {/* end of single item   */}
+
+          {/* 1:32:00 */}
+          <div className={this.state.sent ? 'msg msgAppear' : 'msg'} >Message has been desent</div>
+          <div className="btn" >
+            <button type="submit">submit</button>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const nodemailer = require('nodemailer');
+
+
+const cors = require('cors');
+// what is this never ??????????// 
+
+
+const path = require('path');
+
+const app = express();
+
+
+
+//build static here // 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
+
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
+//static build here //
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+
+});
+
+
+
+app.post('/api/forma', (req, res) => {
+
+  let data = req.body
+  let smtpTransport = nodemailer.createTransport({
+    service: 'Gmail',
+    port: 465,
+    auth: {
+      user: 'doung1118@gmail.com',
+      pass: 'Aa19851118',
+
+    }
+
+  });
+
+
+  let mailOptions = {
+    from: data.email,
+    to: 'doung1118@gmail.com',
+    subject: `message from ${data.name}`,
+    html: `
+
+    <h3>Information </h3>
+    <ul>
+    <li>Name:${data.name}</li>
+    <li>${data.lastname}</li>
+    <li>${data.email}</li>
+     </ul>
+
+     <h3>message</h3>
+     <p>${data.message}</p>
+
+    `
+  };
+
+
+  smtpTransport.sendMail(mailOptions, (error, response) => {
+
+    if (error) {
+      res.send(error)
+    }
+
+    else {
+
+      res.send('success')
+
+    }
+
+  })
+
+
+  smtpTransport.close();
+
+
+})
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("hi")
+  console.log(`server starting at port${PORT}`);
+})
+
+
+
+
+
+// const PORT = process.env.PORT || 300;
+
+// app.listen(PORT, () => {
+
+//   console.log(`server starting at port ${PORT}`);
+// })
